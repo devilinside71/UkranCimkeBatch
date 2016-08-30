cf0a875f3be897ebae1c965cfef70efac0138bc9
Laszlo Tamas <laszlo.devil@gmail.com>
2016-08-30 06:30:52 +0200

    Base with simulation

---
+++ UkranCimkeBatch/Form1.Designer.vb
@@ -0,0 +1,114 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.ComboBoxPrinter = New System.Windows.Forms.ComboBox()
+        Me.ButtonPrint = New System.Windows.Forms.Button()
+        Me.ButtonSzim = New System.Windows.Forms.Button()
+        Me.TextBoxSzim = New System.Windows.Forms.TextBox()
+        Me.TableLayoutPanel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.ColumnCount = 2
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 325.0!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.TableLayoutPanel1.Controls.Add(Me.ComboBoxPrinter, 1, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.ButtonPrint, 1, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.ButtonSzim, 0, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.TextBoxSzim, 0, 0)
+        Me.TableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(0, 0)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 2
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 77.8169!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 22.1831!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(629, 284)
+        Me.TableLayoutPanel1.TabIndex = 0
+        '
+        'ComboBoxPrinter
+        '
+        Me.ComboBoxPrinter.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.ComboBoxPrinter.Font = New System.Drawing.Font("Microsoft Sans Serif", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.ComboBoxPrinter.FormattingEnabled = True
+        Me.ComboBoxPrinter.Location = New System.Drawing.Point(328, 3)
+        Me.ComboBoxPrinter.Name = "ComboBoxPrinter"
+        Me.ComboBoxPrinter.Size = New System.Drawing.Size(298, 41)
+        Me.ComboBoxPrinter.TabIndex = 0
+        '
+        'ButtonPrint
+        '
+        Me.ButtonPrint.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.ButtonPrint.Font = New System.Drawing.Font("Microsoft Sans Serif", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.ButtonPrint.Location = New System.Drawing.Point(328, 224)
+        Me.ButtonPrint.Name = "ButtonPrint"
+        Me.ButtonPrint.Size = New System.Drawing.Size(298, 57)
+        Me.ButtonPrint.TabIndex = 2
+        Me.ButtonPrint.Text = "Nyomtat"
+        Me.ButtonPrint.UseVisualStyleBackColor = True
+        '
+        'ButtonSzim
+        '
+        Me.ButtonSzim.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.ButtonSzim.Location = New System.Drawing.Point(3, 224)
+        Me.ButtonSzim.Name = "ButtonSzim"
+        Me.ButtonSzim.Size = New System.Drawing.Size(319, 57)
+        Me.ButtonSzim.TabIndex = 3
+        Me.ButtonSzim.Text = "Szimuláció"
+        Me.ButtonSzim.UseVisualStyleBackColor = True
+        '
+        'TextBoxSzim
+        '
+        Me.TextBoxSzim.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TextBoxSzim.Font = New System.Drawing.Font("Courier New", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.TextBoxSzim.Location = New System.Drawing.Point(3, 3)
+        Me.TextBoxSzim.Multiline = True
+        Me.TextBoxSzim.Name = "TextBoxSzim"
+        Me.TextBoxSzim.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBoxSzim.Size = New System.Drawing.Size(319, 215)
+        Me.TextBoxSzim.TabIndex = 4
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(629, 284)
+        Me.Controls.Add(Me.TableLayoutPanel1)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Ukrán címke tömeges nyomtatás"
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.TableLayoutPanel1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+    Friend WithEvents ComboBoxPrinter As ComboBox
+    Friend WithEvents ButtonPrint As Button
+    Friend WithEvents ButtonSzim As Button
+    Friend WithEvents TextBoxSzim As TextBox
+End Class
---
+++ UkranCimkeBatch/Form1.vb
@@ -0,0 +1,229 @@
+﻿
+Imports System.Xml
+
+Public Class Form1
+    Public cimkenames As List(Of String) = New List(Of String)
+    Public cimkemegnev As List(Of String) = New List(Of String)
+    Public cimkemeret As List(Of String) = New List(Of String)
+    Public cimkeosztaly As List(Of String) = New List(Of String)
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Call ZebraPrint.LoadPrinters()
+        ComboBoxPrinter.Items.Clear()
+        For Each pr In ZebraPrint.PrinterNames
+            ComboBoxPrinter.Items.Add(pr)
+        Next
+        ComboBoxPrinter.SelectedIndex = 0
+        Call ZebraPrint.LoadLabels()
+        Call LoadCimkes()
+    End Sub
+    Sub ZPLPrint()
+        Dim strPrinter As String
+        Dim strPrintText As String
+        Dim res As String
+        Dim FILE_NAME As String = "CimkekBatch.txt"
+        Dim TextLine As String
+        Dim strCat As String
+        Dim strQty As String
+
+
+
+        strPrinter = ComboBoxPrinter.Text
+        Try
+            strPrinter = ZebraPrint.PrinterWinNames(ComboBoxPrinter.SelectedIndex)
+        Catch ex As Exception
+
+        End Try
+
+
+
+
+
+        If System.IO.File.Exists(FILE_NAME) = True Then
+
+            Dim objReader As New System.IO.StreamReader(FILE_NAME)
+
+            Do While objReader.Peek() <> -1
+                TextLine = objReader.ReadLine()
+                strCat = TextLine.Split(",")(0)
+                strQty = TextLine.Split(",")(1)
+                Debug.Print(strCat & ":" & strQty)
+                strPrintText = ZebraPrint.LabelCodes(0)
+
+                Dim strMegnev As String
+                strMegnev = GetMegnev(strCat)
+                Debug.Print(strMegnev)
+
+                If strMegnev <> vbNullString Then
+                    Dim strMeret As String
+                    strMeret = GetMeret(strCat)
+                    Dim strOszt As String
+                    strOszt = GetOsztaly(strCat)
+
+                    strPrintText = ZebraPrint.LabelCodes(0)
+                    strPrintText = strPrintText.Replace("LABELREF", strCat)
+                    strPrintText = strPrintText.Replace("LABELMEGNEV", ZebraPrint.GetZPLutf8Code(strMegnev))
+                    strPrintText = strPrintText.Replace("MERET", ZebraPrint.GetZPLutf8Code(strMeret))
+
+                    Dim oszt As String
+                    oszt = Trim(strOszt)
+
+                    If oszt = "" Then
+                        oszt = "UA.TR.116"
+                    Else
+                        oszt = ""
+                    End If
+
+                    strPrintText = strPrintText.Replace("OSZTALY", oszt)
+                    strPrintText = strPrintText.Replace("LABELQTY", strQty)
+
+
+                    ' Open the printer dialog box, and then allow the user to select a printer.
+                    res = ZebraPrint.SendStringToPrinter(strPrinter, strPrintText)
+                Else
+
+
+
+                    strPrintText = ZebraPrint.LabelCodes(0)
+                    strPrintText = strPrintText.Replace("LABELREF", strCat)
+                    strPrintText = strPrintText.Replace("MERET", "")
+                    strPrintText = strPrintText.Replace("LABELMEGNEV", "ISMERETLEN")
+
+
+
+                    strPrintText = strPrintText.Replace("OSZTALY", "")
+                    strPrintText = strPrintText.Replace("LABELQTY", "1")
+
+
+                    ' Open the printer dialog box, and then allow the user to select a printer.
+                    res = ZebraPrint.SendStringToPrinter(strPrinter, strPrintText)
+                End If
+
+
+                'Console.WriteLine(strPrintText)
+
+                ' Open the printer dialog box, and then allow the user to select a printer.
+
+            Loop
+        Else
+
+            MessageBox.Show("CimkekBatch.txt nem található")
+        End If
+
+
+        MessageBox.Show("KÉSZ!")
+    End Sub
+
+
+
+    Private Sub ButtonPrint_Click_1(sender As Object, e As EventArgs) Handles ButtonPrint.Click
+        Call ZPLPrint()
+    End Sub
+    Public Function GetMegnev(kod As String) As String
+        GetMegnev = vbNullString
+        For i = 0 To cimkenames.Count - 1
+            If cimkenames(i) = kod Then
+                GetMegnev = cimkemegnev(i)
+            End If
+        Next
+    End Function
+    Public Function GetMeret(kod As String) As String
+        GetMeret = vbNullString
+        For i = 0 To cimkenames.Count - 1
+            If cimkenames(i) = kod Then
+                GetMeret = cimkemeret(i)
+            End If
+        Next
+    End Function
+    Public Function GetOsztaly(kod As String) As String
+        GetOsztaly = vbNullString
+        For i = 0 To cimkenames.Count - 1
+            If cimkenames(i) = kod Then
+                GetOsztaly = cimkeosztaly(i)
+            End If
+        Next
+    End Function
+    Public Sub LoadCimkes()
+        Dim m_xmlr As XmlTextReader
+        'Create the XML Reader
+        m_xmlr = New XmlTextReader("UACimke.xml")
+        'Disable whitespace so that you don't have to read over whitespaces
+        m_xmlr.WhitespaceHandling = WhitespaceHandling.None
+        'read the xml declaration and advance to family tag
+        m_xmlr.Read()
+        'read the family tag
+        m_xmlr.Read()
+        'Load the Loop
+        While Not m_xmlr.EOF
+            'Go to the name tag
+            m_xmlr.Read()
+            'if not start element exit while loop
+            If Not m_xmlr.IsStartElement() Then
+                Exit While
+            End If
+            'Get the Gender Attribute Value
+            cimkenames.Add(m_xmlr.GetAttribute("name"))
+            'Read elements firstname and lastname
+            m_xmlr.Read()
+            'Get the firstName Element Value
+            cimkemegnev.Add(m_xmlr.ReadElementString("megnev"))
+            cimkemeret.Add(m_xmlr.ReadElementString("meret"))
+            cimkeosztaly.Add(m_xmlr.ReadElementString("osztaly"))
+        End While
+        'close the reader
+        m_xmlr.Close()
+    End Sub
+
+    Private Sub ButtonSzim_Click(sender As Object, e As EventArgs) Handles ButtonSzim.Click
+        Call Szimulacio()
+    End Sub
+    Sub Szimulacio()
+        Dim strPrinter As String
+        Dim strPrintText As String
+        Dim res As String
+        Dim FILE_NAME As String = "CimkekBatch.txt"
+        Dim TextLine As String
+        Dim strCat As String
+        Dim strQty As String
+        Dim strSzim As String
+
+
+
+
+        strPrintText = vbNullString
+
+
+        If System.IO.File.Exists(FILE_NAME) = True Then
+
+            Dim objReader As New System.IO.StreamReader(FILE_NAME)
+
+            Do While objReader.Peek() <> -1
+                TextLine = objReader.ReadLine()
+                strCat = TextLine.Split(",")(0)
+                strQty = TextLine.Split(",")(1)
+                Debug.Print(strCat & ":" & strQty)
+
+
+                Dim strMegnev As String
+                strMegnev = GetMegnev(strCat)
+                Debug.Print(strMegnev)
+
+                If strMegnev = vbNullString Then
+                    strPrintText = strPrintText & strCat & vbCrLf
+                End If
+
+
+                'Console.WriteLine(strPrintText)
+
+                ' Open the printer dialog box, and then allow the user to select a printer.
+
+            Loop
+            TextBoxSzim.Text = strPrintText
+        Else
+
+            MessageBox.Show("CimkekBatch.txt nem található")
+        End If
+
+
+        MessageBox.Show("SZIMULÁCIÓ KÉSZ!")
+    End Sub
+End Class
